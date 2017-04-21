62cdcf3598ebcb5c1473baf0c8e7ff51485b86f1
Aaron Goodfellow <amgoodfellow@oakland.edu>
2017-04-21 00:24:24 -0400

    Finished migrating things over to header file

--- src/Image.h
+++ src/Image.h
@@ -4,6 +4,8 @@
 #include <limits>
 
 
+using namespace cv;
+using namespace std;
 
 //Utility method to return which of the three integers
 //was the minimum integer
@@ -18,7 +20,6 @@ int getMin(int a, int b, int c){
     cout << "Some error occured" << endl;
   }
 }
-
 
 
 class Image {
@@ -66,6 +67,11 @@ class Image {
       temp2.release();
       temp3.release();
     }
+
+    /*A dynamic programming approach to seam carving as described in:
+      https://en.wikipedia.org/wiki/Seam_carving#Algorithm
+      This is one of the more reliable methods for carving (among my implementations)
+    */
     void dynamicSeam(){
       Mat temp;
       edges.copyTo(temp);
@@ -84,13 +90,207 @@ class Image {
             array[0][colNum] = temp.at<float>(0, colNum);
           }else{
             if (colNum == 0){
-              array[rowNum][colNum] = temp.at<float>(rowNum, colNum) + returnMin(veryLarge, temp.at<float>(rowNum -1, colNum), temp.at<float>(rowNum -1, colNum + 1)); 
+              array[rowNum][colNum] = temp.at<float>(rowNum, colNum) + min({veryLarge, temp.at<float>(rowNum -1, colNum), temp.at<float>(rowNum -1, colNum + 1)}); 
             }else if (colNum == width -1){
-              array[rowNum][colNum] = temp.at<float>(rowNum, colNum) + returnMin(temp.at<float>(rowNum -1, colNum -1), temp.at<float>(rowNum -1, colNum), veryLarge); 
+              array[rowNum][colNum] = temp.at<float>(rowNum, colNum) + min({temp.at<float>(rowNum -1, colNum -1), temp.at<float>(rowNum -1, colNum), veryLarge}); 
             }else{
               array[rowNum][colNum] = temp.at<float>(rowNum, colNum) + 
-                returnMin(temp.at<float>(rowNum -1, colNum -1), temp.at<float>(rowNum -1, colNum), temp.at<float>(rowNum -1, colNum + 1)); 
+                min({temp.at<float>(rowNum -1, colNum -1), temp.at<float>(rowNum -1, colNum), temp.at<float>(rowNum -1, colNum + 1)}); 
             }
           }
         }
       }
+
+      float minValue = numeric_limits<float>::max();
+      int minIndex = 0;
+      //Weird stuff might happen with .at(height) look here for segfaults
+      for (int colNum = 0; colNum < width; colNum++){
+        if (array[height][colNum] < minValue){
+          minValue = array[height][colNum];
+          minIndex = colNum;
+        }
+      }
+
+      seam.at(height) = minIndex;
+
+      //minIndex is still the colNum
+      
+      for (int rowNum = height - 1; rowNum >= 0; rowNum--){
+        seam.at(rowNum) = minIndex + getMin(array[rowNum][minIndex - 1], array[rowNum][minIndex], array[rowNum][minIndex + 1]);
+        minIndex = seam.at(rowNum);
+      }
+
+      temp.release();
+
+    }
+
+    /*This function was initially going to take 10 random seams using a greedy algorithm
+      appraoch, but as I got carried away, it basically takes a brute force approach to 
+      seam carving. This isn't a very efficient or well-made algorithm, hence the bad
+      variable names ;)
+    */
+    void randomFindSeam(){
+      struct save{
+        int index;
+        int energy;
+      } seams[edges.rows];
+
+      Mat temp2;
+      edges.copyTo(temp2);
+
+      int height = temp2.rows;
+      int width = temp2.cols;
+      int array[height][width];
+      
+      seam.resize(height);
+
+      for (int rowNum = 0; rowNum < height; rowNum++){
+        for (int colNum = 0; colNum < width; colNum++){
+          array[rowNum][colNum] = temp2.at<float>(rowNum, colNum);  
+        }
+      }
+
+      for (int bad = 0; bad < height; bad++){
+        vector<float> bleam(height);
+        int minNum = 255;
+        int minIndex = 0;
+
+        bleam.at(0) = bad;
+
+        for (int rowNum = 1; rowNum < height; rowNum++){
+          minIndex = bleam.at(rowNum - 1);
+          if (bleam.at(rowNum - 1) == 0){
+            bleam.at(rowNum) = minIndex + getMin(256, array[rowNum][minIndex], array[rowNum][minIndex + 1]);
+          }else if (bleam.at(rowNum - 1) == width){
+            bleam.at(rowNum) = minIndex + getMin(array[rowNum][minIndex - 1], array[rowNum][minIndex], 256);
+          }else{
+            bleam.at(rowNum) = minIndex + getMin(array[rowNum][minIndex - 1], array[rowNum][minIndex], array[rowNum][minIndex + 1]);
+          }
+        }
+        float sum;
+        for (auto x : bleam){
+          sum += x;
+        }
+        seams[bad].index = bleam.at(0);
+        seams[bad].energy = sum;
+      }
+      
+      float min = FLT_MAX;
+      float minInd = 0;
+      for (int x = 0; x < height; x++){
+        if (seams[x].energy < min){
+          seams[x].energy = min;
+          minInd = x;
+        }
+      }
+
+      int minNum = 255;
+      seam.at(0) = static_cast<int>(minInd);
+      int minIndex = 0;
+
+      for (int rowNum = 1; rowNum < height; rowNum++){
+        minIndex = seam.at(rowNum - 1);
+        if (seam.at(rowNum - 1) == 0){
+          seam.at(rowNum) = minIndex + getMin(256, array[rowNum][minIndex], array[rowNum][minIndex + 1]);
+        }else if (seam.at(rowNum - 1) == width){
+          seam.at(rowNum) = minIndex + getMin(array[rowNum][minIndex - 1], array[rowNum][minIndex], 256);
+        }else{
+          seam.at(rowNum) = minIndex + getMin(array[rowNum][minIndex - 1], array[rowNum][minIndex], array[rowNum][minIndex + 1]);
+        }
+      }
+      temp2.release();
+
+    }
+
+    /*This was my initial try - it uses a greedy algorithm to find seams, starting from the pixel with
+      the lowest energy in the top row and working on a seam after that. It tends to cut from left to 
+      right too much for my taste. But it's probably the quickest way to find a seam
+    */
+    void findSeam(){
+      Mat temp2;
+      edges.copyTo(temp2);
+
+      int height = temp2.rows;
+      int width = temp2.cols;
+      int array[height][width];
+      
+      seam.resize(height);
+
+      for (int rowNum = 0; rowNum < height; rowNum++){
+        for (int colNum = 0; colNum < width; colNum++){
+          array[rowNum][colNum] = temp2.at<float>(rowNum, colNum);  
+        }
+      }
+
+      int minNum = 255;
+      int minIndex = 0;
+
+      for (int colNum = 0; colNum < width; colNum++){
+        if (array[0][colNum] < minNum){
+          minIndex = colNum;
+          minNum = array[0][colNum];
+        }
+      }
+      seam.at(0) = minIndex;
+
+      for (int rowNum = 1; rowNum < height; rowNum++){
+        minIndex = seam.at(rowNum - 1);
+        if (seam.at(rowNum - 1) == 0){
+          seam.at(rowNum) = minIndex + getMin(256, array[rowNum][minIndex], array[rowNum][minIndex + 1]);
+        }else if (seam.at(rowNum - 1) == width){
+          seam.at(rowNum) = minIndex + getMin(array[rowNum][minIndex - 1], array[rowNum][minIndex], 256);
+        }else{
+          seam.at(rowNum) = minIndex + getMin(array[rowNum][minIndex - 1], array[rowNum][minIndex], array[rowNum][minIndex + 1]);
+        }
+      }
+
+      temp2.release();
+    }
+    /*Originally a utility method for me, I figure I'll leave it in in case
+      anyone ever wants to see what a seam looks like. Also, many other people
+      in their implementations leave a method like this. It just looks kinda cool
+    */
+    void showSeam(){
+      Mat temp;
+      src.copyTo(temp);
+      for (int i = 0; i < temp.rows; i++){
+        for (int j = 0; j < temp.cols; j++){
+          if (seam.at(i) == j){
+            temp.at<Vec3b>(i, j) = Vec3b(0, 0, 255);
+          }
+        }
+      }
+      namedWindow("SEAM");
+      imshow("SEAM", temp);
+      temp.release();
+    }
+
+    /*This method actually removes the seam from the picture. Although one would think this was a simple method,
+      I've had drastically different outcomes depending on how I implement this. This is an issue I'll keep looking
+      at
+    */
+    void removeSeam(){
+      //Move all the pixels to the right of the seam, one pixel to the left
+      
+      for (int rowNum = 0; rowNum < src.rows; rowNum++){
+        for (int colNum = 0; colNum < src.cols -1; colNum++){
+          if (seam.at(rowNum) >= colNum){
+            src.at<Vec3b>(rowNum, colNum) = src.at<Vec3b>(rowNum, colNum +1);
+          }else{
+            src.at<Vec3b>(rowNum, colNum) = src.at<Vec3b>(rowNum, colNum);
+          }
+        }
+      }
+
+      //Resize the image to remove the last column
+      src = src(Rect(0, 0, src.cols-1, src.rows)); 
+
+      namedWindow("REMOVED");
+      imshow("REMOVED", src);
+    }
+
+    float getEdgeValue(int a, int b){
+      return edges.at<float>(a, b);
+    }
+};
+#endif
--- src/classBased.cpp
+++ src/classBased.cpp
@@ -220,66 +220,6 @@ class Image {
       temp2.release();
 
     }
-    void otherFindSeam(){
-      int H = edges.rows, W = edges.cols;
-
-        int dp[H][W];
-        for(int c = 0; c < W; c++){
-            dp[0][c] = (int)edges.at<uchar>(0,c);
-        }
-
-        for(int r = 1; r < H; r++){
-            for(int c = 0; c < W; c++){
-                if (c == 0)
-                    dp[r][c] = min(dp[r-1][c+1], dp[r-1][c]);
-                else if (c == W-1)
-                    dp[r][c] = min(dp[r-1][c-1], dp[r-1][c]);
-                else
-                    dp[r][c] = min({dp[r-1][c-1], dp[r-1][c], dp[r-1][c+1]});
-                dp[r][c] += (int)edges.at<uchar>(r,c);
-            }
-        }
-
-        int min_value = 2147483647; //infinity
-        int min_index = -1;
-        for(int c = 0; c < W; c++)
-            if (dp[H-1][c] < min_value) {
-                min_value = dp[H - 1][c];
-                min_index = c;
-            }
-
-        int path[H];
-        Point pos(H-1,min_index);
-        path[pos.x] = pos.y;
-
-        while (pos.x != 0){
-            int value = dp[pos.x][pos.y] - (int)edges.at<uchar>(pos.x,pos.y);
-            int r = pos.x, c = pos.y;
-            if (c == 0){
-                if (value == dp[r-1][c+1])
-                    pos = Point(r-1,c+1);
-                else
-                    pos = Point(r-1,c);
-            }
-            else if (c == W-1){
-                if (value == dp[r-1][c-1])
-                    pos = Point(r-1,c-1);
-                else
-                    pos = Point(r-1,c);
-            }
-            else{
-                if (value == dp[r-1][c-1])
-                    pos = Point(r-1,c-1);
-                else if (value == dp[r-1][c+1])
-                    pos = Point(r-1,c+1);
-                else
-                    pos = Point(r-1,c);
-            }
-            path[pos.x] = pos.y;
-            copy(&path[0], &path[H], back_inserter(seam)); 
-        }
-    
-    }
     void findSeam(){
       Mat temp2;
       edges.copyTo(temp2);
